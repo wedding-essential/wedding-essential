29670abc604b02ffad9806d56b37e766832655bf
Marco Muser <marco.muser@posteo.de>
2020-07-23 17:01:01 +0200

    add first user routes

--- routes/wedding/user.js
+++ routes/wedding/user.js
@@ -5,10 +5,28 @@ const Wedding = require('../../models/Wedding');
 
 router.get('/:id/users', (req, res) => {
   Wedding
-    .populate('guests')
     .findById(req.params.id)
-    .then(wedding => res.status(200).json(wedding.guests))
+    .populate('guests')
+    .populate('owner')
+    .then(wedding => {
+      const users = wedding.guests
+      users.push(wedding.owner)
+      res.status(200).json(users)
+    })
     .catch(err => res.json(err))
+})
+
+router.put('/:id/user', (req, res) => {
+  const {firstName, lastName, imgName, imgPath, food } = req.body
+  User
+    .findByIdAndUpdate("5f1994193bed0787e8545c5c", {firstName, lastName})
+    .then(user => res.status(200).json(user))
+    .catch(err => res.json(err))
+})
+
+router.delete('/:id/user', async (req, res) => {
+  const user = User.findByIdAndDelete("5f1994193bed0787e8545c5c")
+
 })
 
 module.exports = router;
