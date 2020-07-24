fa2347d6640e31b8d1ad47829166d6204733f90d
Marco Muser <marco.muser@posteo.de>
2020-07-24 17:11:01 +0200

    get wedding object into Home component and fix backend render error

--- client/src/components/home/Dresscode.js
+++ client/src/components/home/Dresscode.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export default class Dresscode extends Component {
   render() {
-    console.log(this.props.dresscode)
     return (
       <div>
         <h2>Dresscode: {this.props.dresscode.category}</h2>
