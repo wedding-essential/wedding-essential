5ecad35b3ad9c8b7b3eae350219064356221a0c3
Marco Muser <marco.muser@posteo.de>
2020-07-25 20:50:35 +0200

    fix auth bugs

--- client/src/components/auth/SignupCouple.js
+++ client/src/components/auth/SignupCouple.js
@@ -1,44 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {signupCouple} from "../../services/auth";
+import { signupCouple } from "../../services/auth";
 
 export default class SignupCouple extends Component {
   state = {
     user: "",
     email: "",
     password: "",
-    message: ""
-  }
+    message: "",
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const target = event.target;
     const name = target.name;
     const value = target.value;
     this.setState({
-      [name]: value
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+
+    signupCouple(email, password).then(data => {
+      if (data.message) {
+        this.setState({
+          message: data.message,
+        })
+      } else {
+        this.props.setUser(data);
+        this.props.history.push('/home');
+      }
     })
   }
 
-  handleSubmit = async event => {
-    try {
-      event.preventDefault();
-      const { email, password } = this.state;
-
-      const signup = await signupCouple(email, password)
-        if (signup.message) {
-          this.setState({
-            message: signup.message,
-          })
-        } else {
-          this.props.setUser(signup);
-        }
-      const redirect = await function() {return this.props.history.push('/home')};
-    }
-    catch(err) {console.log(err)}
-  }
-
   render() {
-
     return (
       <div>
         <div>
@@ -47,13 +44,25 @@ export default class SignupCouple extends Component {
         <h1>Sign Up to Create your Wedding</h1>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" onChange={this.handleChange} value={this.state.email} required></input>
+          <input
+            type="text"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+            required
+          ></input>
           <label>Password</label>
-          <input type="password" name="password" onChange={this.handleChange} value={this.state.password} required></input>
-          {this.state.message && (
-            <p>{this.state.message}</p>
-          )}
-          <button type="submit" value="submit">Next</button>
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.password}
+            required
+          ></input>
+          {this.state.message && <p>{this.state.message}</p>}
+          <button type="submit" value="submit">
+            Next
+          </button>
         </form>
         <h2>
           Already have an account ? <Link to="/login">Log in</Link>
