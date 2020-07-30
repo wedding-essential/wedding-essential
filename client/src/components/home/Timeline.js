0335afb9fe6bca436f48ceff5c4c19b89c1f8123
Marco Muser <marco.muser@posteo.de>
2020-07-30 18:18:36 +0200

    add styling to home

--- client/src/components/home/Timeline.js
+++ client/src/components/home/Timeline.js
@@ -4,31 +4,28 @@ export default class Timeline extends Component {
   render() {
     const eventList = this.props.wedding.events.map((event) => {
       return (
-        <div key={event._id}>
-          <h2>{event.name}</h2>
-          <p>{event.time}</p>
-          <p>{event.location}</p>
+        <div className="event" key={event._id}>
+          <h3>{event.name}</h3>
+          <p>Time: {event.time}</p>
+          <p>Location: {event.location}</p>
           <p>{event.description}</p>
         </div>
       );
     });
     
     return (
-      <>
+      <div className="timeline">
+       <h2>Timeline</h2>
         {this.props.wedding.events.length === 0 ? (
           <>
-            {" "}
-            <h2>Ceremony</h2>
-            <p>10 am</p>
-            <p>Notre Dame de Paris</p>
-            <p>Follow the smoke</p>{" "}
+            <p>Edit wedding to add your events</p>
           </>
         ) : (
             <>
               {eventList}
             </>
           )}
-      </>
+      </div>
     );
   }
 }
--- client/src/components/home/styles.js
+++ client/src/components/home/styles.js
@@ -5,31 +5,95 @@ export const Container = styled.div`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
+  height: 100%;
   margin: auto;
-  height: 100vh;
+  max-width: 768px;
 
   main {
     display: flex;
     flex-flow: column nowrap;
-    flex: 1;
-    align-items: center;
-    justify-content: flex-start;
-    width: 100vw;
+    width: 100%;
+    margin-bottom: 70px;
 
-    .topbar {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-      max-width: 768px;
-      height: 59px;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    }
-
-    h1 {
+    h2 {
       font-weight: 500;
       font-size: 24px;
       line-height: 36px;
       color: ${gold};
     }
+
+    h3 {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 24px;
+      color: ${gold};
+    }
+
+    p {
+      font-size: 14px;
+      text-align: justify;
+    }
+
+    .banner {
+      height: 30vh;
+      background-image: url(${(props) => props.img});
+      background-size: cover;
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-around;
+      align-items: flex-end;
+
+      img {
+        width: 104px;
+        height: 104px;
+        object-fit: cover;
+        border-radius: 50%;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        transform: translateY(50px);
+      }
+
+      .date {
+        background-color: ${gold};
+        padding: 10px 25px;
+        color: white;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 15px;
+        transform: translateY(25px);
+      }
+    }
+    .story {
+      margin: 70px 40px 10px 40px;
+    }
+
+    .timeline,
+    .dresscode,
+    .contact {
+      margin: 10px 40px 10px 40px;
+
+      .event {
+        margin-top: 10px;
+
+        h3 {
+          text-decoration: underline;
+        }
+      }
+      span {
+        color: black;
+        font-size: 16px;
+      }
+    }
+    button {
+      background: ${gold};
+      border: 1px solid ${gold};
+      border-radius: 27px;
+      padding: 8px 4px;
+      margin: 20px 0 15px 0;
+      width: 100px;
+      color: white;
+    }
+
+    .editlink {
+      align-self: center;
+    }
+  }
 `;
