a0b65e9ac1f622affdb0395b3215739d690a5fc0
Tyouxik <paulinemarie.didier@gmail.com>
2020-07-30 14:02:23 +0200

    Hide edit button on guest account gallery and home

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -1,69 +1,83 @@
-import React, { Component } from 'react'
-import AddPicture from '../AddPicture'
-import axios from 'axios'
+import React, { Component } from "react";
+import AddPicture from "../AddPicture";
+import axios from "axios";
 
 export default class Gallery extends Component {
   state = {
     wedding: null,
-    gallery: []
-  }
+    gallery: [],
+  };
 
   componentDidMount() {
-    this.getData()
+    this.getData();
   }
 
   getData = () => {
     const weddingId = this.props.user.wedding;
     axios
       .get(`/api/wedding/${weddingId}`)
-      .then(response => {
-        console.log(response.data)
-        const {gallery} = response.data
+      .then((response) => {
+        console.log(response.data);
+        const { gallery } = response.data;
         this.setState({
           wedding: response.data,
-          gallery
-        })
+          gallery,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   setGallery = (newGallery) => {
-    console.log(newGallery)
+    console.log(newGallery);
     this.setState({
       gallery: newGallery,
     });
   };
 
   deletePicture = (pictID) => {
-    axios.delete(`/api/picture/${pictID}/delete`)
-    .then(response => {
-      console.log(response.data)
-      this.setState({
-        gallery: response.data
+    axios
+      .delete(`/api/picture/${pictID}/delete`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          gallery: response.data,
+        });
+        console.log(response);
       })
-      console.log(response)
-    }). catch (err=> {
-      console.log(err)
-    })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   render() {
-    
     return (
       <>
-      {this.state.gallery.map(photo => {
-        console.log(photo)
-        return (
-          <>
-          <div><img src={photo.imageUrl} style={{width:'200px'}}/></div>
-          <button onClick={() => this.deletePicture(photo._id)} >Delete</button>
-          </>
-        )
-      })}
-      <div>
-        <AddPicture user={this.props.user} gallery={this.state.gallery} setGallery={this.setGallery}/>
-      </div>
+        {this.state.gallery.map((photo) => {
+          console.log(photo);
+          return (
+            <>
+              <div>
+                <img src={photo.imageUrl} style={{ width: "200px" }} />
+              </div>
+              {this.props.user.role === "couple" ? (
+                <button onClick={() => this.deletePicture(photo._id)}>
+                  Delete
+                </button>
+              ) : null}
+            </>
+          );
+        })}
+
+        {this.props.user.role === "couple" ? (
+          <div>
+            <AddPicture
+              user={this.props.user}
+              gallery={this.state.gallery}
+              setGallery={this.setGallery}
+            />
+          </div>
+        ) : null}
       </>
-    )
+    );
   }
 }
