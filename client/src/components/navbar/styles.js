703d04636cf5faa327ec41cfb4e35e9eb6374244
Marco Muser <marco.muser@posteo.de>
2020-07-30 14:16:55 +0200

    make navbar stick to the bottom

--- client/src/components/navbar/styles.js
+++ client/src/components/navbar/styles.js
@@ -2,17 +2,23 @@ import styled from "styled-components";
 import { lightgrey } from "../../styles/colors";
 
 export const Container = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
   width: 100%;
-  max-width: 768px;
-  justify-content: space-evenly;
-  align-items: center;
-  border-top: 2px solid ${lightgrey};
+  position: fixed;
+  background-color: white;
+  bottom: 0;
+  .nav {
+    display: flex;
+    flex-flow: row nowrap;
+    width: 100%;
+    max-width: 768px;
+    justify-content: space-evenly;
+    align-items: center;
+    border-top: 2px solid ${lightgrey};
 
-  img {
-    height: 35x;
-    width: auto;
-    margin: 10px 20px;
+    img {
+      height: 35x;
+      width: auto;
+      margin: 10px 20px;
+    }
   }
 `;
