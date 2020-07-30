f18e629c8c2ee20780af2531ade25b0fa2538641
Marco Muser <marco.muser@posteo.de>
2020-07-30 16:43:15 +0200

    fix styling of profile page

--- client/src/components/profile/styles.js
+++ client/src/components/profile/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   flex-flow: column nowrap;
   align-items: center;
   margin: auto;
-  height: 100vh;
+  height: 100%;
 
   main {
     display: flex;
@@ -14,7 +14,29 @@ export const Container = styled.div`
     flex: 1;
     align-items: center;
     justify-content: flex-start;
-    width: 100vw;
+    width: 100%;
+
+    .topbar-wrapper {
+      position: fixed;
+      top: 0;
+      width: 100%;
+    }
+
+    .topbar {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      max-width: 768px;
+      margin: 0 auto;
+      height: 59px;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      background-color: white;
+    }
+
+    .content {
+      margin: 70px 0;
+    }
 
     img {
       width: 200px;
@@ -48,16 +70,6 @@ export const Container = styled.div`
       font-weight: 500;
       font-size: 16px;
       line-height: 36px;
-    }
-
-    .topbar {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-      max-width: 768px;
-      height: 59px;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
 
     .profile-container {
