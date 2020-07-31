48653d4250725ffe3b29538e3b190f94dd71e05f
Marco Muser <marco.muser@posteo.de>
2020-07-31 10:52:54 +0200

    optimize  styling of guest list

--- client/src/components/guests/Guests.js
+++ client/src/components/guests/Guests.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 import Navbar from "../navbar/Navbar";
 import { Container } from "./styles";
+import omnivore from "../../images/chopped-pork-red.svg";
+import vegetarian from "../../images/blue-cheese.svg";
+import vegan from "../../images/green-carrot.svg"
 
 export default class Guests extends Component {
   state = {
@@ -23,6 +26,15 @@ export default class Guests extends Component {
       })
       .catch((err) => console.log(err));
   };
+  givePicture = (diet) => {
+    if (diet === "omnivore") {
+      return omnivore;
+    } else if (diet === "vegetarian") {
+      return vegetarian;
+    } else if (diet === "vegan") {
+      return vegan;
+    }
+  };
 
   render() {
     if (!this.state.wedding) {
@@ -39,11 +51,11 @@ export default class Guests extends Component {
             <div className="content">
               {this.state.wedding.guests.map((guest) => {
                 return (
-                  <div key={guest._id}>
+                  <div className="guestWrapper" key={guest._id}>
                     <h2>
                       {guest.firstName} {guest.lastName}
                     </h2>
-                    <p>{guest.food}</p>
+                    <img src={this.givePicture(guest.food)}></img>
                   </div>
                 );
               })}
