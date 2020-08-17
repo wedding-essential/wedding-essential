2943ace10ecf883ec256574bb6fba403ff8a0beb
Marco Muser <marco.muser@posteo.de>
2020-08-17 15:11:54 +0200

    set mongoose to useUnifiedTopology

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
 // const favicon      = require('serve-favicon');
 // const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
-
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/wedding-essential', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .set("useUnifiedTopology", true)
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/wedding-essential", {
+    useNewUrlParser: true,
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
-const session = require('express-session');
-const passport = require('passport');
-require('./configs/passport');
+const session = require("express-session");
+const passport = require("passport");
+require("./configs/passport");
 
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -66,19 +70,18 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
-
-app.use('/api/auth', require('./routes/auth/auth.js'));
+app.use("/api/auth", require("./routes/auth/auth.js"));
 
 // app.use('/api/wedding', require('./routes/wedding/gift.js'))
 // app.use('/api/wedding', require('./routes/wedding/post.js'))
-app.use('/api/wedding', require('./routes/wedding/user.js'))
-app.use('/api/wedding', require('./routes/wedding/wedding.js'))
+app.use("/api/wedding", require("./routes/wedding/user.js"));
+app.use("/api/wedding", require("./routes/wedding/wedding.js"));
 
 //Cloudinary
-app.use('/api', require('./routes/file-upload-routes'));
-app.use('/api', require('./routes/wedding/picture'));
+app.use("/api", require("./routes/file-upload-routes"));
+app.use("/api", require("./routes/wedding/picture"));
 
 app.use((req, res) => {
   // If no routes match, send them the React HTML.
