edd4efdfe266ca7c0bf751cda644a050e62f8798
Marco Muser <marco.muser@posteo.de>
2020-08-17 15:20:12 +0200

    delete unnecessary console.logs and clean-up code of backend routes

--- routes/wedding/picture.js
+++ routes/wedding/picture.js
@@ -1,58 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // include the model:
-const Picture = require('../../models/Picture');
-const Wedding = require("../../models/Wedding")
-const User = require("../../models/User")
+const Picture = require("../../models/Picture");
+const Wedding = require("../../models/Wedding");
+const User = require("../../models/User");
 
-router.get('/picture', (req, res, next) => {
-    Picture.find()
-        .then(pictureFromDB => {
-            res.status(200).json(pictureFromDB)
-        })
-        .catch(err => next(err))
-})
-
+router.get("/picture", (req, res, next) => {
+  Picture.find()
+    .then((pictureFromDB) => {
+      res.status(200).json(pictureFromDB);
+    })
+    .catch((err) => next(err));
+});
 
 router.post("/picture/create", (req, res) => {
-    console.log(req.body)
-    Picture.create(req.body).then(DBres => {
-        console.log(DBres)
-        Wedding.findByIdAndUpdate(req.body.wedding, { $push: { gallery: DBres._id } }, { new: true }).populate("gallery").then(wed => {
-            console.log(wed)
-            res.json(wed)
-        })
-    })
-})
+  Picture.create(req.body).then((DBres) => {
+    Wedding.findByIdAndUpdate(
+      req.body.wedding,
+      { $push: { gallery: DBres._id } },
+      { new: true }
+    )
+      .populate("gallery")
+      .then((wed) => {
+        res.json(wed);
+      });
+  });
+});
 
 router.post("/picture/create/banner", (req, res) => {
-    // console.log(req.body)
-    Picture.create(req.body).then(DBres => {
-        // console.log(DBres);
-        console.log('req.body', req.body.user);
-        Wedding.findByIdAndUpdate(req.body.wedding, {bannerImgPath:DBres.imageUrl }, { new: true }).then(wed => {
-            console.log(wed)
-        })
-    })
-    res.json("hello darkness my old friend")
-})
+  Picture.create(req.body).then((DBres) => {
+    Wedding.findByIdAndUpdate(
+      req.body.wedding,
+      { bannerImgPath: DBres.imageUrl },
+      { new: true }
+    ).then((wed) => {});
+  });
+  res.json("banner picture was added");
+});
 
 router.post("/picture/create/profile", (req, res) => {
-    console.log(req.body)
-    Picture.create(req.body).then(DBres => {
-        console.log(DBres);
-        console.log(req.body.user);
-        User.findByIdAndUpdate(req.body.user, {imgPath : DBres.imageUrl }, { new: true }).then(user => {
-            console.log(user)
-        })
-    })
-    res.json("hello darkness my old friend")
-})
+  Picture.create(req.body).then((DBres) => {
+    User.findByIdAndUpdate(
+      req.body.user,
+      { imgPath: DBres.imageUrl },
+      { new: true }
+    ).then((user) => {});
+  });
+  res.json("profile picture was added");
+});
 
 router.delete("/picture/:id/delete", async (req, res) => {
-    const wedding = await Wedding.findByIdAndUpdate(req.user.wedding._id, { $pull: { gallery: req.params.id } }, { new: true }).populate("gallery")
-    await Picture.findByIdAndDelete(req.params.id).pu
-    
-    res.json(wedding.gallery)
-})
+  const wedding = await Wedding.findByIdAndUpdate(
+    req.user.wedding._id,
+    { $pull: { gallery: req.params.id } },
+    { new: true }
+  ).populate("gallery");
+  await Picture.findByIdAndDelete(req.params.id).pu;
+
+  res.json(wedding.gallery);
+});
 module.exports = router;
